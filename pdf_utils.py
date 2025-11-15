ce25335cedf84e084a9a48fc4a399852e5e20e4f
keshav1017 <prasadkeshav003@gmail.com>
2025-11-15 23:41:42 +0530

    pdf utils added

---
+++ pdf_utils.py
@@ -0,0 +1,9 @@
+import PyPDF2
+
+
+def extract_pdf_text(file):
+    reader = PyPDF2.PdfReader(file)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() or ""
+    return text
